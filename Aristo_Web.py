4a33ba0db20068f9399f729a6075a86e23dee307
ItayDar28git config --global user.name ItayDar28 <itay2803@gmail.com>
2021-03-11 14:28:09 +0200

    commited

---
+++ Aristo_Web.py
@@ -0,0 +1,413 @@
+from flask import Flask, render_template, request, redirect, session, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
+from Workers import *
+import time
+
+app = Flask(__name__, template_folder='templates',
+            static_folder=r'C:\Users\itay dar\Desktop\פרויקטים\tender\hello_flask\templates')
+app.secret_key = "tenderly_secret_key"  # secret app for the session to keep data
+app.permanent_session_lifetime = timedelta(minutes=5)  # time untill user forced to log out
+
+'''
+config the connection to mysql database
+'''
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://itda:28031994@127.0.0.1:3306/new_tender'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)  # create connection with database
+
+
+class User(db.Model):
+    '''
+        @Name : User
+        @Do: create table that contain all the users and the relevant data about them in the database
+
+        @ Param:
+                first_name - user first name.
+                last_name - user last name.
+                email: user email address (must be validate by regular expression)
+                password: user password. (must be validate by regular expression)
+    '''
+
+    __tablename__ = 'Users'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(10), nullable=False)
+    # todo references
+    contact_user = db.relationship('Tender', backref=db.backref('User'), lazy=True)
+    # manager_tender = db.relationship('Tender', backref=db.backref('User'), lazy=True)
+    task = db.relationship('UserInTask', backref=db.backref('User'), lazy=True)
+    file = db.relationship('FileInTask', backref=db.backref('User'), lazy=True)
+    comment_in_task = db.relationship('TaskNote', backref=db.backref('User'), lazy=True)
+    user_task_log = db.relationship('TaskLog', backref=db.backref('User'), lazy=True)
+
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+
+class Tender(db.Model):
+    '''
+        @Name : Tender
+        @Do: create table that contain all the Tender and the relevant data about them in the database.
+        @ Param:
+                tid - tender indication number.
+                protocol_number - tender protocol number (for government use).
+                tenders_committee_Type - the area on which the tender rely to such as - ['רכישות','תקשוב','יועצים',...]
+                procedure_type = the type of how the procedure occurs such as
+                                    - ['מכרז פומבי','תיחור סגור','פנייה פומבית','RFI','מכרז חשכ"ל','הצעת מחיר',...]
+                subject: tender subject.
+                department: the departments that create the tender. can be - ['רווחה','מערכות מידע','לוגיסטיקה','לשכה משפטית ',...]
+                start_date: tender start date
+                finish_date: tender estimated finish date
+                contact_user_from_department - reference to a user object from user table.
+                procedure_number - the id of the procedure.
+                tender_manager - the manager of the procedure (get's admin permission)
+    '''
+
+    __tablename__ = "Tenders"
+
+    tid = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    protocol_number = db.Column(db.VARCHAR(20))
+    tenders_committee_Type = db.Column(db.VARCHAR(20))
+    procedure_type = db.Column(db.VARCHAR(20))
+    subject = db.Column(db.VARCHAR(250), nullable=False)
+    department = db.Column(db.VARCHAR(50))
+    start_date = db.Column(db.DateTime(255), nullable=False)
+    finish_date = db.Column(db.DateTime(255), nullable=False)
+    contact_user_from_department = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+    tender_manager = db.Column(db.VARCHAR(50),nullable=False)
+    # todo references
+    task = db.relationship('Task', backref='Tender', lazy=True)
+
+    def __init__(self, protocol_number, tenders_committee_Type, procedure_type, subject, department, start_date,
+                 finish_date, contact_user_from_department, tender_manager):
+        self.protocol_number = protocol_number
+        self.tenders_committee_Type = tenders_committee_Type
+        self.procedure_type = procedure_type
+        self.subject = subject
+        self.department = department
+        self.start_date = start_date
+        self.finish_date = finish_date
+        self.contact_user_from_department = contact_user_from_department
+        self.tender_manager = tender_manager
+
+
+class Task(db.Model):
+    """
+    task_id = primary_key
+    tid = tender id
+    odt = open date time (removed from "blocked")
+    deadline = needed to be done until
+    finish = finished in actual fact
+    status = ["open", "close", "blocked", "on progress"]
+    subject = short text(50)
+    description = longer text(120)
+    """
+    __tablename__ = "Tasks"
+    task_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    tender_id = db.Column(db.Integer, db.ForeignKey("Tenders.tid"))
+    odt = db.Column(db.DateTime(255), nullable=False)
+    deadline = db.Column(db.DateTime(255), nullable=False)
+    finish = db.Column(db.DateTime(255))
+    # status = db.Column(db.VARCHAR(50),db.CheckCostraint('status in ["open", "close", "blocked", "on progress"]'))  # todo
+    status = db.Column(db.VARCHAR(50))
+    subject = db.Column(db.VARCHAR(50))
+    description = db.Column(db.VARCHAR(120))
+    # todo references
+    task_users = db.relationship('UserInTask', backref='Tasks', lazy=True)
+    task_logs = db.relationship('TaskLog', backref='Tasks', lazy=True)
+    task_notes = db.relationship('TaskNote', backref='Tasks', lazy=True)
+    task_files = db.relationship('FileInTask', backref='Tasks', lazy=True)
+
+
+
+    def __init__(self, tender_id, odt, deadline, finish, status, subject, description):
+        self.tender_id = tender_id
+        self.odt = odt
+        self.deadline = deadline
+        self.finish = finish
+        self.status = status
+        self.subject = subject
+        self.description = description
+
+
+class TaskLog(db.Model):
+    """
+    task_id = foreign key
+    init_time= the time the log was created
+    description = needed to be set and interpreted:
+        user_id added u user_id = "נעה לוי הוסיפה את אבי כהן"
+        user_id added f file_id = "נעה לוה הוסיפה קובץ למשימה"
+        user_id eddited description = "נעה לוי שינתה את תיאור המשימה"
+        user_id eddited status = "נעה לוי שינתה את סטטוס המשימה"
+        ....
+    """
+    __tablename__ = "TasksLogs"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.id"), primary_key=True)
+    init_time = db.Column(db.DateTime(255), primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey("Tasks.task_id"))
+    description = db.Column(db.VARCHAR(255), nullable=False)
+
+    def __init__(self,user_id, task_id, init_time, description):
+        self.user_id = user_id
+        self.task_id = task_id
+        self.init_time = init_time
+        self.description = description
+
+
+class TaskNote(db.Model):
+    """
+    description
+    """
+    __tablename__ = "TasksNotes"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.id"), primary_key=True)
+    time = db.Column(db.DateTime(255), primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey("Tasks.task_id"))
+    note = db.Column(db.VARCHAR(120), nullable=False)
+
+    def __init__(self, user_id, time, task_id, note):
+        self.user_id = user_id
+        self.time = time
+        self.task_id = task_id
+        self.note = note
+
+
+class UserInTask(db.Model):
+    '''
+        @Name : UserInTask
+        @Do: create table that contain for each user all the tasks that he is assign
+        @ Param:
+                task_id - user indication number.
+                user_id - task id.
+                Permissions - the user's premissions
+    '''
+
+    __tablename__ = "UsersInTasks"
+
+    task_id = db.Column(db.Integer, db.ForeignKey('Tasks.task_id'), primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True, nullable=False)
+    # permissions = db.Column(db.Varchar(10),db.CheckCostraint('permissions in ["god","admin","editor","viewer"]'))
+    permissions = db.Column(db.VARCHAR(10))
+
+    def __init__(self, task_id, user_id, permissions):
+        self.task_id = task_id
+        self.user_id = user_id
+        self.permissions = permissions
+
+
+class FileInTask(db.Model):
+    '''
+        @Name : FilesInTasks
+        @Do: for each file
+        @ Param:
+                file_id - file indication number.
+                file_name - the name of the file
+                file - the file itself - saved as largebinary.
+                task_id = the task where the file publishd
+                user_id - the user that published the task
+    '''
+
+    __tablename__ = "FilesInTasks"
+    file_id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    file_name = db.Column(db.VARCHAR(250))
+    file_data = db.Column(db.LargeBinary)
+    task_id = db.Column(db.Integer, db.ForeignKey('Tasks.task_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+
+    def __init__(self, file_id, file_name, file_data,task_id,user_id):
+        self.file_id = file_id
+        self.file_name = file_name
+        self.file_data = file_data
+        self.task_id = task_id
+        self.user_id = user_id
+
+
+class TaskDependency(db.Model):
+    '''
+        @Name : FilesInTasks
+        @Do: for each file
+        @ Param:
+            blocked: the task that depend in other task.
+            blocking: the task that block other task.
+    '''
+
+    __tablename__ = "TasksDependencies"
+
+    blocked = db.Column(db.Integer, db.ForeignKey('Tasks.task_id'), primary_key=True, nullable=False)
+    # blocking = db.Column(db.Integer,db.CheckCostraint('blocking!=blocked'), db.ForeignKey('Task.task_id'), primary_key=True, nullable=False)
+    blocking = db.Column(db.Integer, db.ForeignKey('Tasks.task_id'), primary_key=True, nullable=False)
+
+    def __init__(self, blocked, blocking):
+        self.blocked = blocked
+        self.blocking = blocking
+
+@app.route("/")
+@app.route("/home")
+def home():
+    print("hello flask")
+    return render_template("home.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        req = request.form['tab']
+        if req == "sign-in":
+            print("here")
+            email = request.form['email_con']
+            password = request.form['pass_con']
+            users = User.query.all()
+            for user in users:
+                if user.password == password and user.email == email:
+                    print("user in database - ready to move to profile")
+                    flash("login successfully")
+                    session["user"] = user.email
+                    return redirect(url_for("user"))
+            print("here")
+            flash("סיסמא או מייל שגויים - לחץ על הרשמה")
+        else:
+            print("not in sign in - in sign up")
+            first_name = request.form['first_name']
+            last_name = request.form['last_name']
+            new_pass = request.form['new_pass']
+            verify_pass = request.form['verify_pass']
+            new_email = request.form['new_email']
+            try:
+                if new_pass == verify_pass:
+                    if validate_email(new_email):
+                        if validate_password(new_pass):
+                            print("user is about to enter db")
+                            user = User(first_name, last_name, new_email, new_pass)
+                            try:
+                                db.session.add(user)
+                                db.session.commit()
+                                flash("הרשמתך נקלטה בהצלחה, מיד תועברי לעמוד הפרופיל שלך")
+                                time.sleep(5)
+                                return redirect(url_for("user"))
+                            except Exception as e:
+                                print(e)
+                                print("user cannot enter database")
+                                db.session.rollback()
+                                flash("תהליך יצירת משתמש נכשל - נסה שנית")
+                        else:
+                            flash("סיסמא חייבת להכיל אות גדולה, אות קטנה וספרה")
+                    else:
+                        flash("כתובת אימייל לא תקינה")
+                else:
+                    flash("אימות סיסמא נכשל")
+            except Exception as e:
+                print(e)
+    print("return")
+    return render_template("login.html")
+
+
+@app.route("/user")
+def user():
+    return render_template("user.html")
+
+
+@app.route("/tenders", methods=["POST", "GET"])
+def tenders():
+    if request.method == "POST":
+        session.permanent = True
+        try:
+            req = request.form['user']
+            tender_to_show = Tender.query.filter_by(tid=req).first()
+            contact_guy = User.query.filter_by(id=tender_to_show.contact_user_from_department).first()
+            return render_template("tender.html", tender=tender_to_show,contact_guy=contact_guy)
+        except Exception as e:
+            try:
+                req = request.form['new_tender']
+                return redirect(url_for("newTender"))
+            except Exception as e:
+                print(e)
+                print("here you need to sort/filter")
+                try:
+                    req = ('subject',request.form['subject'])
+                    print("req")
+                    tender = Tender.query.order_by(Tender.subject.desc()).all()
+                    print(tender.query.first())
+                    return render_template("home.html")
+                except:
+                    try:
+                        req = ('subject',request.form['start_date'])
+                        Tender.query.order_by(Tender.start_date.desc()).all()
+                    except:
+                        req = ('subject',request.form['department'])
+                        Tender.query.order_by(Tender.department.desc()).all()
+
+
+
+            # values = function_for_sorting(request,Tender,db)
+            #     return render_template("tenders.html", values=values, len=len(values))
+    values = return_values(User,Tender)
+    return render_template("tenders.html", values=values, len=len(values))
+
+
+@app.route("/newTender", methods=["POST", "GET"])
+def newTender():
+    if request.method == 'POST':
+        session.permanent = True
+        try:
+            protocol_number = request.form['protocol_number']
+            tenders_committee_Type = request.form['tenders_committee_Type']
+            procedure_type = request.form['procedure_type']
+            subject = request.form['subject']
+            department = request.form['department']
+            try:
+                start_date = str_to_datetime(request.form['start_date'])
+                finish_date = str_to_datetime(request.form['finish_date'])
+            except:
+                start_date = datetime.now()
+                finish_date = datetime.now()
+            try:
+                contact_user_from_department = int(request.form['contact_user_from_department'])
+            except:
+                flash("יש להזין את שם הגורם מטעם היחידה")
+            tender_manager = request.form['tender_manager']
+            tender = Tender(protocol_number,tenders_committee_Type,procedure_type,
+                            subject,department,start_date,finish_date,
+                            contact_user_from_department,tender_manager)
+            try:
+                db.session.add(tender)
+                db.session.commit()
+                flash("מכרז נוצר בהצלחה - מיד תועבר לעמוד המכרז")
+                contact_guy = User.query.filter_by(id=tender.contact_user_from_department).first()
+                return render_template("tender.html",tender=tender,contact_guy=contact_guy)
+            except Exception as e:
+                print(e)
+                db.session.rollback()
+                flash("יש להזין תאריכי התחלת וסיום המכרז")
+        except Exception as e:
+            print(e)
+    return render_template("newTender.html")
+
+
+@app.route("/tender/<tender>", methods=["POST", "GET"])
+def tender(tender):
+    print("heree")
+    return render_template("tender.html", tender=tender)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
---
+++ Workers.py
@@ -0,0 +1,234 @@
+from faker import Faker
+import random
+from datetime import datetime
+import numpy as np
+from tqdm import tqdm
+
+
+def enter_tenders_to_db(Tenders,db,number_of_tenders_to_add):
+    fake = Faker()
+    tenders_committee_Type_lst = ['רכישות', 'תקשוב', 'יועצים']
+    procedure_type_lst = ['מכרז פומבי', 'תיחור סגור', 'פנייה פומבית', 'RFI', 'מכרז חשכ"ל', 'הצעת מחיר']
+    department_lst = ['רווחה', 'מערכות מידע', 'לוגיסטיקה', 'לשכה משפטית ']
+    cont_users = ['צחי יעקב', 'עופר שווקי', 'יהל ישראל', 'יגאל בלפור', 'משה כהן', 'אבי סלומון', 'שחר סינואני',
+                  'ליאורה קחטן', 'שרונה בן שיטרית', 'ספיר כלפון', 'סמי גריידי', 'אפרת מצליח', 'אדוה הלוי',
+                  'גורם מטעם היחידה', 'עוזי נעים', 'נטע ראובני', 'אלי דווידי', "טלי צ'רני", 'לבנת כהן', 'מיכל שפירא',
+                  'יוני קראוני', 'מיסא זועבי', 'דניאלה יפת', 'נועה רוזנפלד', 'קרן נועם', 'יהודה כהן', 'הילה שלום',
+                  'לימור ניזרי', 'יהונתן קוליץ', 'מירב כהן', 'דליה בנימין', 'אבי סולומון', 'מנשה מלול', 'ירון זוהר',
+                  'ליבנת כהן']
+    subjects_lst = ['מתן שירותי הובלה וסבלות', 'עו"ד מתחום דיני העבודה', 'הדר דפנה מזנונים', 'מכרז לשדרוג תשתיות חשמל',
+                    'התאמות בגין לקויות למידה', 'שיפוץ בית הדין השרעי', "אחסון מטלטלין חלק ב'", 'שירותי שמאות רכב',
+                    'מכרז שמאות לציוד שנתפס', 'שמאות חפצי אומנות', 'מכירת חפצי אומנות וזהב.', 'ימי גיבוש 2020',
+                    'לינות קאדים', 'מכרז הדפסות', 'מכרז לרכישת מוצרים AEM', 'מכרז שליחויות', 'מכרז תחזוקה',
+                    'מרכז קלט נתונים רשל"ה', 'מתח נמוך', 'פטנטים חיפוש ידע', 'צילום מסמכים', 'צילום מסמכים', 'ט"ו בשבט',
+                    'שימור ידע', 'שירותי בינוי תשתיות', 'תווי שי', 'מתן שירותי ביקורת פנים', 'מכרז בנקים',
+                    'לסניגוריה הציבורית"', 'חדר כושר לבניין החדש', 'מזנון בשרי', 'מזנון חלבי', 'ניקיון בניין ראשי',
+                    'ריהוט וציוד נייד', 'ציוד חשמלי למטבחונים', 'צמחיה בתוך המבנה', 'תמונות ואמצעים אסתטיים',
+                    'יועץ ארגונומי', 'מולטימדיה - השלמת ציוד', 'עבודות בינוי ושיפוץ', 'צלונים']
+    for i in range(number_of_tenders_to_add):
+        protocol = "".join(random.choices([str(i) for i in range(0, 10)], k=8))
+        comm = random.choice(tenders_committee_Type_lst)
+        proc = random.choice(procedure_type_lst)
+        subject = random.choice(subjects_lst)
+        depar = random.choice(department_lst)
+        start_date = fake.past_date()
+        finish_date = fake.future_date()
+        cont = random.choice([i for i in range(1,50)])
+        manager = random.choice(cont_users)
+        tender = Tenders(protocol, comm, proc, subject, depar, start_date, finish_date, cont, manager)
+        try:
+            db.session.add(tender)
+            db.session.commit()
+            print('tender added succussfully')
+        except Exception as e:
+            print(e)
+            print('cannot add tender')
+            break
+    return
+
+
+
+def datetime_to_str(date):
+    return datetime.strftime(date,"%d/%m/%Y")
+
+def str_to_datetime(string):
+    return datetime.strptime(string,'%Y-%d-%m')
+
+def return_values(User,Tender,filter_by=None,order_by=None):
+    if filter_by is not None:
+        tenders = Tender.query.filter_by(tid=filter_by).first()
+    elif order_by is not None:
+        tender = Tender.query.oreder_by(f"{order_by} desc")
+    else:
+        tenders = Tender.query.all()
+    days = [(t.finish_date - datetime.now()).days for t in tenders]
+    values = []
+    for i,tender in enumerate(tenders):
+        tender.start_date = datetime_to_str(tender.start_date)
+        tender.finish_date = datetime_to_str(tender.finish_date)
+        # tender_contact_guy = User.query.join(Tender).filter_by()
+        # print(tender_contact_guy)
+        # con_guy_name = tender_contact_guy.first_name + ' ' + tender_contact_guy.last_name
+        # print(con_guy_name)
+        values.append((tender.protocol_number,tender.tenders_committee_Type,
+                       tender.procedure_type,tender.subject,tender.department,
+                       tender.contact_user_from_department,tender.tender_manager,
+                       tender.start_date,tender.finish_date,days[i],tender.tid))
+    return values
+
+
+
+def validate_email(mail):
+    import re
+    email_re = re.compile("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$")
+    if len(email_re.findall(mail)) == 1:
+        return True
+    else:
+        return False
+
+def validate_password(password):
+    import re
+    pass_re = re.compile("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
+    if len(pass_re.findall(password)) == 1:
+        return True
+    else:
+        return True
+
+
+def enter_fake_users_to_db(number_of_users,db,Users):
+    fake = Faker()
+    for i in range(number_of_users):
+        name = fake.name().split()
+        f_name = name[0]
+        l_name = name[1]
+        email = f"{f_name}_{l_name}@gmail.com"
+        password = "".join([str(np.random.randint(0,10)) for _ in range(8)])
+        user = Users(first_name=f_name,last_name=l_name,email=email,password=password)
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            continue
+    return
+
+def delete_users_from_db(number_to_delete,db,Users):
+    users = Users.query.all()
+    for i,user in enumerate(users):
+        if i == len(users):
+            return
+        elif i == number_to_delete:
+            return
+        else:
+            try:
+                db.session.delete(user)
+                db.session.commit()
+            except:
+                db.session.rollback()
+
+
+def enter_fake_tasks_to_db(Tender,Task,db,num):
+    statuses = ['חסום','הושלם','בעבודה']
+    fake = Faker()
+    tenders = Tender.query.all()
+    for i in range(num):
+        tender_id = random.choice([i for i in range(1,len(tenders))])
+        odt = fake.past_date()
+        dead_line = fake.future_date()
+        status = random.choice(statuses)
+        subject = f"{random.choice(range(1,60))}כתיבת פרק מספר -"
+        description = f"זהו תיאור המשימה"
+        task = Task(tender_id=tender_id,odt=odt,deadline=dead_line,finish=None,status=status,subject=subject,description=description)
+        # print(task.subject)
+        try:
+            db.session.add(task)
+            db.session.commit()
+        except Exception as e:
+            print("cannot enter user to db")
+            print(e)
+            db.session.rollback()
+
+
+def enter_fake_task_logs(db,User,Task,TaskLog,num):
+    fake = Faker()
+    users = User.query.all()
+    task = Task.query.all()
+    users_id = [i for i in range(1,len(users))]
+    tasks_id = [i for i in range(1,len(task))]
+    for i in range(num):
+        user = random.choice(users_id)
+        task_id = random.choice(tasks_id)
+        init_time = fake.date_time_this_month()
+        description = f"זהו תיאור הפעולה בתוך המשימה"
+        log = TaskLog(user,task_id,init_time,description)
+        try:
+            db.session.add(log)
+            db.session.commit()
+            print("log enter succ")
+        except Exception as e:
+            print("cannot enter user to db")
+            print(e)
+            pass
+
+
+def enter_fake_task_noted(db,User,Task,TaskNote,num):
+    fake = Faker()
+    users = User.query.all()
+    task = Task.query.all()
+    users_id = [i for i in range(1,len(users))]
+    tasks_id = [i for i in range(1,len(task))]
+    for i in range(num):
+        user = random.choice(users_id)
+        task_id = random.choice(tasks_id)
+        init_time = fake.date_time_this_month()
+        description = f"זוהי הערה"
+        note = TaskNote(user,init_time,task_id,description)
+        try:
+            db.session.add(note)
+            db.session.commit()
+            print("log enter succ")
+        except Exception as e:
+            print("cannot enter user to db")
+            print(e)
+            pass
+
+
+def enter_fake_user_in_task(db,User,Task,UserInTask,num):
+    prem = ['god','admin','editor','viewer']
+    users = User.query.all()
+    task = Task.query.all()
+    users_id = [i for i in range(1,len(users))]
+    tasks_id = [i for i in range(1,len(task))]
+    tafus = []
+    for i in tqdm(range(num)):
+        user = random.choice(users_id)
+        task_id = random.choice(tasks_id)
+        if (user,task_id) in tafus:
+            print("already inside")
+            continue
+        else:
+            tafus.append((user,task_id))
+        permission = random.choice(prem)
+        user_in_task = UserInTask(task_id,user,permission)
+        try:
+            db.session.add(user_in_task)
+            db.session.commit()
+        except Exception as e:
+            continue
+
+
+def drop_all_tables(db):
+    db.session.drop_all()
+
+def fill_db(num,db,User,Tender,Task,TaskLog,TaskNote,UserInTask):
+    enter_fake_users_to_db(num,db,User)
+    enter_tenders_to_db(Tender,db,6*num)
+    enter_fake_tasks_to_db(Tender,Task,db,12*num)
+    enter_fake_task_logs(db,User,Task,TaskLog,18*num)
+    enter_fake_task_noted(db,User,Task,TaskNote,18*num)
+    enter_fake_user_in_task(db,User,Task,UserInTask,num*6)
+
+
+
+def function_for_sorting(request,Tender,db):
+    pass
